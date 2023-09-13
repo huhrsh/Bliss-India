8ad9b564dd43588cf06f6c2514bf8c35a30dec0b
Harsh Jain <harshj2010@gmail.com>
2023-09-13 11:26:48 +0530

    Changing environment.js

--- config/environment.js
+++ config/environment.js
@@ -10,34 +10,9 @@ const accessLogStream=rfs.createStream('access.log',{
     path:logDirectory
 })
 
-const development={
-    name: 'development',
-    asset_path:'./assets',
-    session_cookie_key: '29DD93AED29EAA4413D48585AA25B',
-    db:'BlissIndia',
-    smtp:{
-        service:'gmail',
-        host:'smtp.gmail.com',
-        port: 587,
-        secure:false,
-        auth:{
-            user:'blissofficialindia',
-            pass:'ikfsjkoxlwtatozc'
-        }
-    },
-    google_client_id: "66736520931-dcd643omimgjuof7kf7tc9j1iq8nro4s.apps.googleusercontent.com",
-    google_client_secret: "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",
-    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
-    morgan:{
-        mode:'dev',
-        options:{stream:accessLogStream}
-    }
-}
-
 const production={
     name:'production',
     asset_path:process.env.asset_path,
-    // asset_path:'./assets',
     session_cookie_key: process.env.session_cookie_key,
     db:process.env.db,
     smtp:{
@@ -60,5 +35,4 @@ const production={
     
 }
 
-// module.exports=eval(process.env.environment)==undefined?development:eval(process.env.environment);
-module.exports=development;
+module.exports=eval(process.env.environment)==undefined?development:eval(process.env.environment);
